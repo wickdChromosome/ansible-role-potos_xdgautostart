91dda1d275d4d0756611a8583821a8fe307bf0f8
wickdChromosome <capsule.edu@gmail.com>
2024-05-02 17:54:50 +0200

    feat(): Add wrapper script for scripts to be executed

--- files/disable_script.sh
+++ files/disable_script.sh
@@ -1,6 +1,6 @@
 # Disable this script
 script_name=$(basename "$0")
 mkdir -p "$HOME/.config/autostart/"
-cp "/etc/xdg/autostart/$script_name.desktop" "$HOME/.config/autostart/$script_name.desktop"
-sed -i 's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g' "$HOME/.config/autostart/$script_name.desktop"
-chmod +x "$HOME/.config/autostart/$script_name.desktop"
+cp "/etc/xdg/autostart/$script_name_wrapper.sh.desktop" "$HOME/.config/autostart/$script_name_wrapper.sh.desktop"
+sed -i 's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g' "$HOME/.config/autostart/$script_name_wrapper.sh.desktop"
+chmod +x "$HOME/.config/autostart/$script_name_wrapper.sh.desktop"
