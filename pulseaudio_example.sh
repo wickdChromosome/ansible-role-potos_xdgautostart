249bb1fe6497fb1a40aac0de3aa0c6b3d7956918
wickdChromosome <capsule.edu@gmail.com>
2024-05-15 11:17:11 +0200

    fix(): Remove dup script and populate CODEOWNERS

--- pulseaudio_example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Set default sink volume for user
-potos_pulseaudio_defsink=$( pactl info | awk '/Default Sink:/ {print $3}' )
-pactl set-sink-mute $potos_pulseaudio_defsink 0 # Make sure its unmuted
-pactl set-sink-volume $potos_pulseaudio_defsink "{{ potos_pulseaudio_default_volume }}"
